843f710a3a81d9c0dc9625b48a3edade55244517
kinjingi <kinjingi@users.noreply.github.com>
2026-07-16 11:49:55 +0530

    Hardcode Firebase credentials for Vercel deployment

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "dummy",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "dummy",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "dummy",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "dummy",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "dummy",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "dummy",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "dummy"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCnguQ_nUsBlhtMTCpRs8yqATsHIGj2sL0",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "invoice-56fe7.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "invoice-56fe7",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "invoice-56fe7.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "494619749416",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:494619749416:web:70359f7676108e7232d91d",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-5NCHRCS6B1"
 };
 
 let app;
